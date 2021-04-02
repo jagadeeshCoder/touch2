102f43c143af619ab22eb10b705d3fe711591f38
jagadeeshCoder <74042387+jagadeeshCoder@users.noreply.github.com>
2021-04-02 14:12:11 +0530

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,21 @@
+
+
+
+let button;
+
+function setup() {
+  createCanvas(400,400);
+  button = createButton("BOOP!");
+  button.size(200,100);
+  button.position(10,10);
+ 
+}
+
+function draw (){
+  background(255);
+}
+
+
+  
+
+
